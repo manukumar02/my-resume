9fe3969cf8a8f67901a6a9bec737c29279ec09fd
Manu <manukumar02@gmail.com>
2024-04-21 18:50:48 +0530

    Added lock screen ui

--- src/app/lock/page.tsx
+++ src/app/lock/page.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image';
 import React from 'react';
 
 const LockPage = () => {
-  return <div>LockPage</div>;
+  return (
+    <div className='flex flex-row gap-4 h-full w-full'>
+      <div className="col1 w-2/3 bg-gradient-to-r from-slate-200 via-slate-500 h-full">
+        <h1>hello</h1>
+      </div>
+      <div className="col2 w-1/3">world</div>
+    </div>
+  )
 };
 
 export default LockPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import '../styles/globals.css'
 
 export default function Home() {
   redirect('/home');
